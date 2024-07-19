2c1dd0668a76dd69ee6270d32444f56d9f74c99d
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-19 18:44:47 -0400

    Adding extra pictures for testing

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -320,3 +320,56 @@ from public_tests import *
 model_test(model)
 logistic_regression_model = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
 print("========================================")
+
+# Example of a picture that was wrongly classified.
+index = 1
+plt.imshow(test_set_x[:, index].reshape((num_px, num_px, 3)))
+plt.show()
+print ("y = " + str(test_set_y[0,index]) + ", the predicted picture is a \"" + classes[int(logistic_regression_model['Y_prediction_test'][0,index])].decode("utf-8") +  "\" picture.")
+
+# Plot learning curve (with costs)
+costs = np.squeeze(logistic_regression_model['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(logistic_regression_model["learning_rate"]))
+plt.show()
+
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+
+# Choice of learning rate
+"""
+In order for Gradient Descent to work you must choose the learning rate wisely. The learning rate 𝛼 determines how rapidly 
+we update the parameters. If the learning rate is too large we may "overshoot" the optimal value. 
+Similarly, if it is too small we will need too many iterations to converge to the best values. That's why it is crucial 
+to use a well-tuned learning rate.
+"""
+for lr in learning_rates:
+    print ("Training a model with learning rate: " + str(lr))
+    models[str(lr)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=lr, print_cost=False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
+
+for lr in learning_rates:
+    plt.plot(np.squeeze(models[str(lr)]["costs"]), label=str(models[str(lr)]["learning_rate"]))
+
+plt.ylabel('cost')
+plt.xlabel('iterations (hundreds)')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+plt.show()
+
+# Trying the modal with a different picture
+my_image = "cat_in_iran.jpg"
+
+# We preprocess the image to fit your algorithm.
+fname = "cat.jpeg"
+image = np.array(Image.open(fname).resize((num_px, num_px)))
+plt.imshow(image)
+image = image / 255.
+image = image.reshape((1, num_px * num_px * 3)).T
+my_predicted_image = predict(logistic_regression_model["w"], logistic_regression_model["b"], image)
+plt.show()
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
