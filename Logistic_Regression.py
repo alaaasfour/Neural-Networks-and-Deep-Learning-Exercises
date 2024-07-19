60ab9af936d3328d269f7caf35477684d01dd138
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-19 12:39:36 -0400

    Adding the .h5 datasets files

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -8,4 +8,14 @@ import h5py
 import spicy
 from PIL import Image
 from scipy import ndimage
+from lr_utils import load_dataset
+from public_tests import *
+
+# Loading the data (cat/non-Cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+# Example of a picture
+index = 24
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
