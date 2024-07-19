b81d25c234f27e48f1f95bc7d8a150b87aa730a0
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-19 16:58:55 -0400

    Ex 4: Initializing Parameters

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -96,3 +96,29 @@ output = sigmoid(x)
 print(output)
 sigmoid_test(sigmoid)
 print("========================================")
+
+"""
+Exercise 4: Initializing Parameters of the Neural Network
+This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0
+Argument: dim is the size of the w vector we want (number of parameters)
+Returns: 
+    w: initialized vector of shape (dim, 1)
+    b: initialized scalar (corresponds to the bias) of type float
+"""
+print("Exercise 4: Initializing Parameters")
+print("==========")
+def initialize_with_zeros(dim):
+    w = np.zeros((dim, 1))
+    b = 0.0
+    return w, b
+
+dim = 2
+w, b = initialize_with_zeros(dim)
+
+assert type(b) == float
+print ("w = " + str(w))
+print ("b = " + str(b))
+
+initialize_with_zeros_test_1(initialize_with_zeros)
+initialize_with_zeros_test_2(initialize_with_zeros)
+print("========================================")
