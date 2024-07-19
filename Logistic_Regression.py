c414a5b5fcc39acf083fc67a6718692bb78d11f8
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-19 16:51:32 -0400

    More tests for the sigmoid function

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -91,5 +91,8 @@ def sigmoid(z):
     return s
 
 print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+x = np.array([0.5, 0, 2.0])
+output = sigmoid(x)
+print(output)
 sigmoid_test(sigmoid)
 print("========================================")
