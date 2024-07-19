b0c6914e5e5234f069bacba23eccd4aebffaf315
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-19 12:26:08 -0400

    Creating the Logistic Regression file and Import the Packages

---
+++ Logistic_Regression.py
@@ -0,0 +1,11 @@
+# Logistic Regression with a Neural Network Mindset
+
+# Packages
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import h5py
+import spicy
+from PIL import Image
+from scipy import ndimage
+
