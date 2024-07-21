ca9a415495c348c9df593b8aef50e97f52fe050c
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-20 23:11:10 -0400

    Ex 1: Initializing Parameters Function

--- Building_Deep_Neural_Network.py
+++ Building_Deep_Neural_Network.py
@@ -16,3 +16,30 @@ from public_tests import *
 import copy
 np.random.seed(1)
 
+"""
+Exercise 1: Initializing Parameters
+We will create and initialize the parameters of the 2-layer neural network.
+The model structure will look like this: LINEAR -> RELU -> LINEAR -> SIGMOID
+Argument:
+    n_x: size of the input layer
+    n_h: size of the hidden layer
+    n_y: size of the output layer
+    
+Returns:
+    parameters: python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+"""
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(1)
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {'W1': W1, 'b1':b1, 'W2': W2, 'b2': b2}
+
+    return parameters
+
