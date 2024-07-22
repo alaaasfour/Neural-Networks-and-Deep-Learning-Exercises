689a20a4a5e7f003b96c77832c91a593c710fb71
Alaa Asfour <alaa.asfour94@outlook.com>
2024-07-22 00:12:43 -0400

    Ex 4: L-layer Neural Network

--- Deep_Neural_Network_Application.py
+++ Deep_Neural_Network_Application.py
@@ -172,4 +172,56 @@ predictions_test = predict(test_x, test_y, parameters)
 
 # We can notice that the 2-layer neural network has better performance (72%) than the logistic regression implementation.
 
+"""
+Exercise 4: L-layer Neural Network
+We're going to build the L-layer deep neural network
 
+
+Arguments:
+    X: input data, of shape (n_x, number of examples)
+    Y: true "label" vector (containing 1 if cat, 0 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+    
+Returns:
+    parameters: parameters learnt by the model. They can then be used to predict.
+"""
+### CONSTANTS ###
+layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost = False):
+    np.random.seed(1)
+    costs = []  # keep track of cost
+    # Parameters initialization.
+    parameters = initialize_parameters_deep(layers_dims)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        AL, caches = L_model_forward(X, parameters)
+
+        # Compute cost.
+        cost = compute_cost(AL, Y)
+
+        # Backward propagation.
+        grads = L_model_backward(AL, Y, caches)
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        # Print the cost every 100 iterations
+        if print_cost and i % 100 == 0 or i == num_iterations - 1:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if i % 100 == 0 or i == num_iterations:
+            costs.append(cost)
+
+    return parameters, costs
+
+parameters, costs = L_layer_model(train_x, train_y, layers_dims, num_iterations = 1, print_cost = False)
+
+print("Cost after first iteration: " + str(costs[0]))
+
+L_layer_model_test(L_layer_model)
--- helper_mthods.py
+++ helper_mthods.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-
 def sigmoid(Z):
     A = 1 / (1 + np.exp(-Z))
     cache = Z
